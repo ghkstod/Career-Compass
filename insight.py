62b1a98bd09389bd947ceb1ea69f4b4d089bd230
hanwool <na990312@naver.com>
2024-03-27 15:48:46 +0900

    add

--- insight.py
+++ insight.py
@@ -8,10 +8,10 @@ def insight():
     data = pd.read_csv(file_path, index_col=0, encoding='CP949')
 
     # Streamlit 앱 시작
-    st.title('연도별 구인인원수 변화')
+    st.markdown('<h1 style = "color : #2ec4b6; font-size : 50px; text-align : left;">취업시장 동향(Beta)</h1>', unsafe_allow_html=True)
 
     # 직종 선택 드롭다운
-    selected_job = st.selectbox('직종을 선택하세요', data.index)
+    selected_job = st.selectbox('확인해보고 싶은 직종을 선택해!', data.index)
 
     # 선택한 직종에 대한 그래프 표시
     if selected_job:
@@ -24,5 +24,5 @@ def insight():
         st.plotly_chart(fig)
 
     # 데이터프레임 출력 (옵션)
-    if st.checkbox('데이터 보기'):
+    if st.checkbox('데이터 확인하러 가기'):
         st.write(data)
